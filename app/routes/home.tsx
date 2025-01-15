af0d293e2d4b83e9c1868d8794c6a4b9bed33a11
Sophia Pung <sophiagracepung@gmail.com>
2025-01-15 11:03:34 -0800

    cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 import MenuBar from "../components/MenuBar/MenuBar";
 
 export function meta({}: Route.MetaArgs) {

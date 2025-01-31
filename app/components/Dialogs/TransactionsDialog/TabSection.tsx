6d7b36ef066de693a8c825d0b84b78dd644f3469
Sophia Pung <sophiagracepung@gmail.com>
2025-01-31 15:10:32 -0600

    client section in Transactions tab

--- app/components/Dialogs/TransactionsDialog/TabSection.tsx
+++ app/components/Dialogs/TransactionsDialog/TabSection.tsx
@@ -192,6 +192,235 @@ const TabSection = () => {
             </div>
           </div>
         );
+
+      case "Client":
+        return (
+          <div className="client-content">
+            <div className="client-layout">
+              <div className="client-left-column">
+                {/* Client Search Section */}
+                <div className="client-search">
+                  <label>Look Up Client</label>
+                  <input
+                    type="text"
+                    className="input-field"
+                    defaultValue="Daalmans, Jacco"
+                  />
+                </div>
+
+                {/* Name Fields */}
+                <div className="client-details">
+                  <div className="contact-form-group">
+                    <label>Title/First/Last</label>
+                    <div className="name-inputs">
+                      <input type="text" className="input-field title-input" />
+                      <input
+                        type="text"
+                        className="input-field first-input"
+                        defaultValue="Jacco"
+                      />
+                      <input
+                        type="text"
+                        className="input-field last-input"
+                        defaultValue="Daalmans"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="contact-form-group">
+                    <label>Spouse</label>
+                    <input type="text" className="input-field" />
+                  </div>
+
+                  <div className="contact-form-group">
+                    <label>Company</label>
+                    <input type="text" className="input-field" />
+                  </div>
+
+                  <div className="contact-form-group">
+                    <label>Email Address</label>
+                    <input
+                      type="text"
+                      className="input-field"
+                      defaultValue="jadaalmans@hotmail.com"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="client-right-column">
+                <div className="top-group">
+                  <div className="tax-id-group">
+                    <label>Tax ID</label>
+                    <input type="text" className="input-field tax-input" />
+                    <button className="action-button">New Client</button>
+                  </div>
+                </div>
+                <div className="bottom-group">
+                  <div className="expires-group">
+                    <label>Expires</label>
+                    <input type="text" className="input-field date-input" />
+                    <span>/</span>
+                    <input type="text" className="input-field date-input" />
+                    <select className="input-field time-select">
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Address Sections */}
+            <div className="addresses-container">
+              {/* Mailing Address */}
+              <div className="address-section">
+                <div className="address-type">
+                  <span>Mailing address type:</span>
+                  <select className="input-field">
+                    <option>Mailing</option>
+                  </select>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 1</label>
+                  <input
+                    type="text"
+                    className="input-field"
+                    defaultValue="Trocaderostraat 9"
+                  />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 2</label>
+                  <input type="text" className="input-field" />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 3</label>
+                  <input type="text" className="input-field" />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>City/State</label>
+                  <div className="city-state-inputs">
+                    <input
+                      type="text"
+                      className="input-field city-input"
+                      defaultValue="Maastricht"
+                    />
+                    <input
+                      type="text"
+                      className="input-field state-input"
+                      defaultValue="NL"
+                    />
+                  </div>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Zip/Country</label>
+                  <div className="zip-country-inputs">
+                    <input
+                      type="text"
+                      className="input-field zip-input"
+                      defaultValue="6213"
+                    />
+                    <input
+                      type="text"
+                      className="input-field country-input"
+                      defaultValue="Netherlands"
+                    />
+                  </div>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Phone</label>
+                  <div className="phone-input-group">
+                    <input
+                      type="text"
+                      className="input-field"
+                      defaultValue="00316 231 843 30"
+                    />
+                    <button className="action-button">Ship to Here</button>
+                  </div>
+                </div>
+              </div>
+
+              {/* Shipping Address */}
+              <div className="address-section">
+                <div className="address-type">
+                  <span>Shipping address type:</span>
+                  <select className="input-field">
+                    <option>Shipping</option>
+                  </select>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 1</label>
+                  <input
+                    type="text"
+                    className="input-field"
+                    defaultValue="Stockholmstraat 52"
+                  />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 2</label>
+                  <input type="text" className="input-field" />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Address 3</label>
+                  <input type="text" className="input-field" />
+                </div>
+
+                <div className="contact-form-group">
+                  <label>City/State</label>
+                  <div className="city-state-inputs">
+                    <input
+                      type="text"
+                      className="input-field city-input"
+                      defaultValue="Hengelo"
+                    />
+                    <input
+                      type="text"
+                      className="input-field state-input"
+                      defaultValue="NL"
+                    />
+                  </div>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Zip/Country</label>
+                  <div className="zip-country-inputs">
+                    <input
+                      type="text"
+                      className="input-field zip-input"
+                      defaultValue="7559"
+                    />
+                    <input
+                      type="text"
+                      className="input-field country-input"
+                      defaultValue="Netherlands"
+                    />
+                  </div>
+                </div>
+
+                <div className="contact-form-group">
+                  <label>Phone</label>
+                  <div className="phone-input-group">
+                    <input
+                      type="text"
+                      className="input-field"
+                      defaultValue="00316 231 843 30"
+                    />
+                    <button className="action-button">Ship to Here</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
       default:
         return null;
     }
